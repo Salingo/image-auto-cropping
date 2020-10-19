1ee13da14ac90e41046f10ed96bbbdc812084c19
Salingo <salingo@hotmail.com>
2020-10-19 13:43:22 +0800

    add group cropping mode

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Make sure you have installed Pillow:
 `pip install Pillow`
 
 To use, simply run:
-`python crop.py IMAGE_PATH`
+`python crop.py <path_to_images> <0 or 1>`, 0 for individual cropping, 1 for group cropping (by the minimum boundary of the images).
--- crop.py
+++ crop.py
@@ -1,21 +1,54 @@
 import os
 import sys
+import numpy as np
 from PIL import Image, ImageChops
 
-CROP_FOLDER = sys.argv[1]
+CROP_FOLDER = sys.argv[-2]
+TRIM_SET = int(sys.argv[-1])
 
-def trim(im):
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0))) # Get the background color from the top left pixel
-    diff = ImageChops.difference(im, bg)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
+def trim_single(img):
+	bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
+	diff = ImageChops.difference(img, bg)
+	bbox = diff.getbbox()
+	img_cropped = img.crop(bbox)
+	return img_cropped
+
+''' Trim all images in the SOURCE_FOLDER by the common minimum bbox '''
+def trim_set(imgs):
+	bboxs = []
+	for img in imgs:
+		bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
+		diff = ImageChops.difference(img, bg)
+		bbox = diff.getbbox()
+		bboxs.append(bbox)
+	bboxs = np.array(bboxs)
+	common_bbox = np.zeros((4))
+	common_bbox[0] = np.min(bboxs, axis=0)[0]
+	common_bbox[1] = np.min(bboxs, axis=0)[1]
+	common_bbox[2] = np.max(bboxs, axis=0)[2]
+	common_bbox[3] = np.max(bboxs, axis=0)[3]
+	imgs_cropped = []
+	for img in imgs:
+		img = img.crop(common_bbox)
+		imgs_cropped.append(img)
+	return imgs_cropped
+
 
 if __name__ == "__main__":
-	files = os.listdir(CROP_FOLDER)
-	for file in files:
-		if file.split('.')[1] == 'png' or file.split('.')[1] == 'jpg' or file.split('.')[1] == 'jpeg':
-			image = Image.open(CROP_FOLDER+file) # The image to be cropped
-			new_image = trim(image)
-			new_image.save(CROP_FOLDER+file) # Overwrite original files
-			print("Cropped: "+file)
+	file_names = os.listdir(CROP_FOLDER)
+	images = []
+	images_name = []
+	for file_name in file_names:
+		if file_name.split('.')[1] == 'png' or file_name.split('.')[1] == 'jpg' or file_name.split('.')[1] == 'jpeg':
+			image = Image.open(os.path.join(CROP_FOLDER, file_name))
+			if TRIM_SET:
+				images.append(image)
+				images_name.append(file_name)
+			else:
+				image_cropped = trim_single(image)
+				image_cropped.save(os.path.join(CROP_FOLDER, file_name))
+				print("\ncropped: "+file_name)
+	if TRIM_SET:
+		images_cropped = trim_set(images)
+		for i in range(len(images_cropped)):
+			images_cropped[i].save(os.path.join(CROP_FOLDER, images_name[i]))
